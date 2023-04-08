71cdfb6fc4daf8c1293c44f91807b1c0b56d2d24
William Hobson <will.hobson85@gmail.com>
2023-04-08 19:02:53 -0400

    Write footer with links to Github and LinkedIn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import Navbar from '@/components/Navbar'
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import Footer from '@/components/Footer'
 
 export default function RootLayout({
   children,
@@ -19,6 +20,7 @@ export default function RootLayout({
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
